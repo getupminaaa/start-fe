c77d5f239861091c758cc8d4baac884b3bb99de6
getupminaaa <getupmina0405@gmail.com>
2021-03-17 22:23:06 +0900

    2주차 2차시 수업내용

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,31 @@
+/*
 console.log('app.js');
 
-console.log(1+2);
+번수---
+var name = '강민아';
+var Name = '강민아';
+
+console.log(name);
+console.log(Name);
+
+var var = 'test'; 예약어는 변수명으로 사용불가
+
+주석
+//은 한 줄 
+
+
+데이터 타입---
+var count = 1;
+console.log(count);
+
+var isMan = true; //bool
+var isMan = 'true'; //String
+
+console.log(isMan);
+*/
+
+function log(str){
+    console.log(str);
+}
+
+log('hello')
