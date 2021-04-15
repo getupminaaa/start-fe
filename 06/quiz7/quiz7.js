79e2743e082edf1cf8183081717d5c5b72ec9d65
getupminaaa <getupmina0405@gmail.com>
2021-04-15 23:55:55 +0900

    directory 수정 및 오류 수정 (해결 못함)

--- 06/quiz7/quiz7.js
+++ 06/quiz7/quiz7.js
@@ -4,40 +4,44 @@ const $point = document.getElementById("point");
 const $life = document.getElementById("life");
 let isRedBoxAlive = true;
 
-function placeBoxRandom(){
-    $redBox.style.left = `${Math.random()* ($box.clientWidth-$redBox.clientWidth)}px`;
-    $redBox.style.top = `${Math.random()* ($box.clientHeight-$redBox.clientHeight)}px`;
+function placeBoxRandom() {
+    $redBox.style.left = `${Math.random() * ($box.clientWidth - $redBox.clientWidth)}px`;
+    console.log($redBox.style.left)
+    $redBox.style.top = `${Math.random() * ($box.clientHeight - $redBox.clientHeight)}px`;
     isRedBoxAlive = true;
 }
 
-function getPoint(){
-    $point.textContent = parseInt($point.textContent) +1;
+function getPoint() {
+    $point.textContent = parseInt($point.textContent) + 1;
 }
-function lostLife(){
-    if($life.textContent == 0){
-        alertGameOver
+function lostLife() {
+    if (parseInt($life.textContent) == 0) {
+        alertGameOver();
         return;
     }
-    $life.textContent = parseInt($life.textContent)-1;
+    $life.textContent = parseInt($life.textContent) - 1;
+
 }
-function alertGameOver(){
-        alert("game over");
+function alertGameOver() {
+    alert("game over");
 }
 
-function missRedBox(){
-    if(isRedBoxAlive){
+function missRedBox() {
+    if (isRedBoxAlive) {
         lostLife();
-    }else{
         placeBoxRandom();
     }
+    isRedBoxAlive = true
+
 }
-function catchRedBox(){
-    if(!isRedBoxAlive){
-        getPoint()
-        isRedBoxAlive = false;
+function catchRedBox() {
+    isRedBoxAlive = false;
+    if (!isRedBoxAlive) {
+        getPoint();
+        placeBoxRandom();
     }
 }
 
 
 $box.addEventListener("click", missRedBox);
-$redBox.addEventListener("click",catchRedBox);
+$redBox.addEventListener("click", catchRedBox);
