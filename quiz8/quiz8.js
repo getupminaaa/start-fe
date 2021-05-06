f42630bcf5e9324c66ca4168697b1bdc5a06aa86
getupminaaa <getupmina0405@gmail.com>
2021-05-06 16:47:42 +0900

    8-1 수업 및 퀴즈 08

---
+++ quiz8/quiz8.js
@@ -0,0 +1,38 @@
+const $btn = document.getElementById('btn');
+const $box = document.getElementById('box');
+const index = -20;
+let min = 0;
+let max = 0;
+let result = 0;
+let count = index;
+let timerId = null;
+
+function getValue() {
+  min = document.getElementById('min').value;
+  max = document.getElementById('max').value;
+  $box.textContent = min;
+}
+
+function setResult(resultNum) {
+  $box.innerHTML = resultNum + count;
+  count += 1;
+  if (count === 1) {
+    clearInterval(timerId);
+    $btn.disabled = false;
+    count = index;
+  }
+}
+
+function getRandomNum(minNum, maxNum) {
+  const minX = Math.ceil(minNum);
+  const maxY = Math.floor(maxNum);
+  result = Math.floor(Math.random() * (maxY - minX) + minX);
+  timerId = setInterval(setResult, 50, result);
+  $btn.disabled = true;
+}
+
+$btn.addEventListener('click', (event) => {
+  getValue();
+  getRandomNum(min, max);
+  $box.innerHTML = result;
+});
